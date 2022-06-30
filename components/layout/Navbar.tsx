d93e15f33c93e71d5be06fbd0d83bf9af6076cca
MichalKonvic <51423281+MichalKonvic@users.noreply.github.com>
2022-06-30 10:27:09 +0200

    Initial commit

---
+++ components/layout/Navbar.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import styles from './../../styles/layout.module.css'
+const Navbar = () => {
+    return (
+        <div className={styles.navbar}>
+
+        </div >
+    )
+}
+
+export default Navbar
