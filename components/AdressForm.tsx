64d698d0508ce18df5053fb780b0fdab968ff25d
MichalKonvic <51423281+MichalKonvic@users.noreply.github.com>
2022-08-17 12:15:47 +0200

    adress form inputs

--- components/AdressForm.tsx
+++ components/AdressForm.tsx
@@ -8,6 +8,10 @@ const AdressForm = () => {
                 Address
             </label>
             <input type="text" name="adress" id="adress" placeholder='1234 Main St' />
+            <label htmlFor="note">
+                Note
+            </label>
+            <input type="text" name="note" id="note" placeholder='...' />
         </form>
     )
 }
