69f0eca0c8155dc8cd1f72f45455310628ad1095
MichalKonvic <51423281+MichalKonvic@users.noreply.github.com>
2022-08-19 14:38:44 +0200

    cache hook

---
+++ lib/cache.ts
@@ -0,0 +1,19 @@
+type SetStateAction<S> = S | ((prevVal: S)=>S);
+interface cacheHookI<T> {
+        cacheData: () => T,
+    setCache: (value: SetStateAction<T>) => void;
+};
+function cacheHook<T>(intialValue:T,key:string):cacheHookI<T> {
+    const setCache = (value:SetStateAction<T>) => {
+        if (!global[key]) {
+            global[key] = intialValue;
+        }
+        const newState:T = (typeof value === "function") ? value(global[key]) : value;
+        global[key] = newState;
+    }
+    const cacheData = ():T => global[key];
+    return { cacheData, setCache };
+}
+
+export default cacheHook;
+export type {cacheHookI,SetStateAction}
